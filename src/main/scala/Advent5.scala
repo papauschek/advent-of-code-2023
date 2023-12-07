eb7c17cfa2dc5264f266641c4f0e7569ea52c7fb
Christian Papauschek <christian@journiapp.com>
2023-12-07 23:34:24 +0100

    advent 5 solution part 2

--- src/main/scala/Advent5.scala
+++ src/main/scala/Advent5.scala
@@ -8,52 +8,86 @@ object Advent5:
     val lines = Files.readString(Path.of("./inputs/input5.txt")).linesIterator.toSeq
 
     val seeds = lines.head.split(':').last.trim.split(' ').map(_.toLong).toSeq
+    val seedRanges = seeds.grouped(2).toSeq.map(group => Range(group.head, group.last))
 
     val rawMaps = lines.drop(2).mkString("\n").split("\n\n").toSeq
 
     def parseMapping(rawMap: String): Mapping = {
       val mapLines = rawMap.linesIterator.toSeq
       val name = mapLines.head.split(' ').head // e.g. "seed-to-soil"
-      val ranges = mapLines.drop(1).map(parseRange)
+      val ranges = mapLines.drop(1).map(parseDestination)
       Mapping(name, ranges)
     }
 
-    def parseRange(rawRange: String): Range = {
+    def parseDestination(rawRange: String): Destination = {
       val parts = rawRange.split(' ')
       val destinationStart = parts(0).toLong
       val sourceStart = parts(1).toLong
       val sourceCount = parts(2).toLong
-      Range(destinationStart, sourceStart, sourceCount)
+      Destination(destinationStart, Range(sourceStart, sourceCount))
     }
 
     val maps: List[Mapping] = rawMaps.map(parseMapping).toList
 
-    @tailrec
-    def resolveLocation(seed: Long, remainingMaps: List[Mapping] = maps): Long = {
+    def resolveLocation(inputRange: Range, remainingMaps: List[Mapping] = maps): Seq[Range] = {
       if (remainingMaps.isEmpty) {
-        seed
+        Seq(inputRange)
       } else {
         val map = remainingMaps.head
-        val nextNumber = map.ranges.find(range => range.sourceStart <= seed && range.sourceEnd > seed) match {
-          case Some(range) => seed + range.destinationOffset
-          case _ => seed
+
+        var remainingRanges = Seq(inputRange)
+        var outputRanges = Seq.empty[Range]
+        map.destinations.foreach {
+          destination =>
+            remainingRanges = remainingRanges.flatMap {
+              remainingRange =>
+                val (intersection, remainder) = remainingRange.intersection(destination.source)
+                val offset = intersection.offset(destination.destinationOffset)
+                outputRanges = outputRanges :+ offset
+                remainder
+            }
         }
-        resolveLocation(nextNumber, remainingMaps.tail)
+
+        val outputs = outputRanges.filter(_.count > 0) ++ remainingRanges
+        outputs.flatMap(range => resolveLocation(range, remainingMaps.tail))
       }
     }
 
-    println(seeds.map(resolveLocation(_)).min)
+    println(seedRanges.flatMap(resolveLocation(_)).map(_.start).min) // 136096660
 
   }
 
-  case class Mapping(name: String, ranges: Seq[Range])
+  case class Mapping(name: String, destinations: Seq[Destination])
 
-  case class Range(destinationStart: Long,
-                   sourceStart: Long,
-                   sourceCount: Long) {
+  case class Destination(destinationStart: Long,
+                         source: Range) {
 
-    val sourceEnd: Long = sourceStart + sourceCount
-    val destinationOffset: Long = destinationStart - sourceStart
+    val destinationOffset: Long = destinationStart - source.start
+
+  }
+
+  case class Range(start: Long, count: Long) {
+
+    // non-inclusive
+    val end: Long = start + count
+
+    /** return intersection range and remainder ranges (if any), not contained in the other range */
+    def intersection(other: Range): (Range, Seq[Range]) = {
+      if (other.start >= end || other.end <= start) {
+        (Range(0, 0), Seq(this)) // no intersection
+      } else {
+        val intersectionStart = Math.max(start, other.start)
+        val intersectionEnd = Math.min(end, other.end)
+        val intersection = Range(intersectionStart, intersectionEnd - intersectionStart)
+        val remainder = Seq(
+          Range(start, intersectionStart - start),
+          Range(intersectionEnd, end - intersectionEnd)
+        ).filter(_.count > 0)
+        (intersection, remainder)
+      }
+    }
+
+    def offset(offset: Long): Range = copy(start = start + offset)
 
   }
 
